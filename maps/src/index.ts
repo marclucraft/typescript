001047fa54f53a9ca51840b676a6ac13ed522e1d
Marc Lucraft <marclucraft@gmail.com>
2021-03-01 22:47:46 +0000

    Maps app complete

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,12 +1,10 @@
-/// <reference types="@types/googlemaps" />
-
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
-new google.maps.Map(document.getElementById('map'), {
-	zoom: 10,
-	center: {
-		lat: 0,
-		lng: 0,
-	},
-});
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
+customMap.addMarker(company);
