37a1f8498e37a8ae88692b32af22d282420f6016
Marc Lucraft <marclucraft@gmail.com>
2021-03-07 19:03:34 +0000

    Tests

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('hi there');
+console.log('hello there');
+var log = function (msg) {
+    console.log(msg);
+};
+log('this is a test');
+log('another test');
+log('My name is marc lucraft');
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,9 @@
-console.log('hi there');
+console.log('hello there');
+
+const log = (msg: string) => {
+	console.log(msg);
+};
+
+log('this is a test');
+log('another test');
+log('My name is marc lucraft');
