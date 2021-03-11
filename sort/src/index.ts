f671bdf151f803702408c4a139d2efa23552b7d4
Marc Lucraft <marclucraft@gmail.com>
2021-03-11 16:05:18 +0000

    sync

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -2,12 +2,12 @@ import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 
-// const numberscollection = new NumbersCollection([10, 3, -5, 0]);
-// const sorter = new Sorter(numberscollection);
-// sorter.sort();
-// console.log(numberscollection.data);
+const numberscollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter1 = new Sorter(numberscollection);
+sorter1.sort();
+console.log(numberscollection.data);
 
 const characterscollection = new CharactersCollection('Keeley');
-const sorter = new Sorter(characterscollection);
-sorter.sort();
+const sorter2 = new Sorter(characterscollection);
+sorter2.sort();
 console.log(characterscollection.data);
